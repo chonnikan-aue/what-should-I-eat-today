ca13b45b11ce3066a24a8de571b16557af71551f
Chonnikan <chonnikan.aue@gmail.com>
2022-12-01 10:13:47 +0700

    set state for recipe

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,16 +1,37 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Recipe = () => {
   const foodId = useParams().foodId;
+  let [recipe, setRecipe] = useState([]);
 
   useEffect(() => {
     axios
       .get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)
       .then((res) => {
         let data = res.data.meals[0];
-        console.log(data);
+        let ingredientArr = [];
+        for (let i = 1; i <= 20; i++) {
+          let measure = data[`strMeasure${i}`];
+          let ingredient = data[`strIngredient${i}`];
+          if (measure && ingredient) {
+            //to catch some data that being null (null can't call trim())
+            let item = `${measure.trim()} ${ingredient.trim()}`.trim();
+            if (item !== "") {
+              ingredientArr.push(item);
+            }
+          }
+        }
+        setRecipe({
+          foodPic: data.strMealThumb,
+          foodName: data.strMeal,
+          foodCountry: data.strArea,
+          foodCategory: data.strCategory,
+          foodYoutubeLink: data.strYoutube,
+          foodIngredient: ingredientArr,
+          foodInstructions: data.strInstructions.split(". "),
+        });
       })
       .catch((err) => {
         console.log(err);
--- src/components/SeeRecipeButton/SeeRecipeButton.js
+++ src/components/SeeRecipeButton/SeeRecipeButton.js
@@ -1,19 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./SeeRecipeButton.css";
 
 const SeeRecipeButton = (props) => {
-  props.setHeaderText(`You chose ${props.chosenFood.foodName}!`);
+  useEffect(() => {
+    props.setHeaderText(`You chose ${props.chosenFood.foodName}!`);
+  }, []);
 
   return (
     <Link to={`/recipe/${props.chosenFood.foodId}`}>
-      <Button
-        className="see-recipe-btn"
-        type="button"
-        variant="info"
-        size="lg"
-      >
+      <Button className="see-recipe-btn" type="button" variant="info" size="lg">
         See Recipe
       </Button>
     </Link>
